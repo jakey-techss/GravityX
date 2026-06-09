5c2e8a1ba5feb5167132546e35a38c8ace57d21a
jakey.techss <joshuauzokwe12@gmail.com>
2026-06-08 22:40:07 -0400

    Airbound HTML & CSS Done Completely - Need to write JS for responsiveness and animate jump method and appropriate the drop method

--- 2D.js
+++ 2D.js
@@ -2,43 +2,51 @@ const canvas = document.getElementById("planet-canvas")
 const ctx = canvas.getContext("2d");
 canvas.width = canvas.clientWidth
 canvas.height = canvas.clientHeight
-ctx.imageSmoothingEnabled = true
+ctx.imageSmoothingEnabled = false
 
-const planetImage = new Image()
-planetImage.src = "assets/specialPlanets/earth.png"
+const alien = new Image()
+alien.src = "public/assets/Dude_Monster.png"
 
-const ball = {
-    x: 400,
-    y: 100,
-    radius: 30,
-    velocityY: 0,
-    gravity: 0.9
-};
+alien.onload = () => {
+    ctx.scale(2, 2)
+    const ball = {
+        image: alien,
+        x: 280,
+        y: 260,
+        radius: alien.height * 10 + 100,
+        velocityY: 0,
+        gravity: 0.9
 
-function animate() {
+    };
 
-    requestAnimationFrame(animate);
+    ctx.beginPath();
+    ctx.drawImage(alien, ball.x, ball.y)
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    function animateDrop() {
 
-    // Physics
-    ball.velocityY += ball.gravity;
-    ball.y += ball.velocityY;
+        requestAnimationFrame(animateDrop);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Floor collision
-    if(ball.y + ball.radius > canvas.height) {
-        ball.y = canvas.height - ball.radius;
-        ball.velocityY *= -0.8;
+        // Physics
+        ball.velocityY += ball.gravity;
+        ball.y += ball.velocityY;
+
+        // Floor collision
+        if (ball.y + ball.radius > canvas.height) {
+            ball.y = canvas.height - ball.radius;
+            ball.velocityY *= -1;
+        }
+
+        // Draw
+        ctx.beginPath();
+
+        ctx.drawImage(alien, ball.x, ball.y)
+
     }
 
-    // Draw
-    ctx.beginPath();
-    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-    ctx.fillStyle = "orange";
-    ctx.fill();
 }
 
-animate();
+
 
 
 
